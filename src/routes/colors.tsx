3b056133e1a2684ec91639b76d7964296156b678
Naresh Sikha <naresh.sikha1@forgeglobal.com>
2024-09-16 05:22:12 -0700

    works

---
+++ src/routes/colors.tsx
@@ -0,0 +1,16 @@
+export default function Index() {
+  return (
+    <main>
+      <div class="light">
+        <div class="text-text-default bg-layout-background-strong h-[256px] w-[256px]">
+          Text
+        </div>
+      </div>
+      <div class="dark">
+        <div class="text-text-default bg-layout-background-strong h-[256px] w-[256px]">
+          Text
+        </div>
+      </div>
+    </main>
+  );
+}
